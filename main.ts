79c0df1a51cd6d96c30729b74d6c2cdaec559bdd
sandra-dot <81304862+sandra-dot@users.noreply.github.com>
2023-01-06 02:47:19 +0300

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,5 @@
+basic.forever(function () {
+	Zojak World Wide is a full-service music publisher & digital music distribution company. We enter into contractual relationships with songwriters, music composers, artists, indie labels & producers for the commercial exploitation of music. We service publishing and distribution to over 200 digital service providers including majors such as iTunes/Apple Music, Spotify, Vevo, Tik Tok, Pandora, Tidal, Google, YouTube, Amazon, and more..
+
+    Zojak World Wide has been innovative in marketing and promotion and is a pioneer of animated videos through social media and maintains a strong online presence for independent music. Many artists and producers across the world sign with Zojak to maintain their copyright ownership and to take part in innovative marketing campaigns to keep them at the forefront of the music industry!
+})
